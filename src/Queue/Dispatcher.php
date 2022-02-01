6e0b89b2f6e84b4c0d5bcbd8e1e38195919dd569
butschster <butschster@gmail.com>
2022-02-01 19:09:13 +0300

    Fixes cs

--- src/Queue/Dispatcher.php
+++ src/Queue/Dispatcher.php
@@ -37,6 +37,7 @@ final class Dispatcher implements DispatcherInterface
     }
 
     /**
+     * @return void
      * @throws \Spiral\RoadRunner\Jobs\Exception\JobsException
      */
     public function serve()
