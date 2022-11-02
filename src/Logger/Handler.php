750a2069919fcb9e70f08a00078975bc0b072da0
kastahov <kastahov1021@gmail.com>
2022-11-02 17:06:10 +0200

    Add formatter to handler and tests

--- src/Logger/Handler.php
+++ src/Logger/Handler.php
@@ -4,36 +4,38 @@ declare(strict_types=1);
 
 namespace Spiral\RoadRunnerBridge\Logger;
 
+use Monolog\Formatter\FormatterInterface;
+use Monolog\Formatter\LineFormatter;
 use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger;
 use RoadRunner\Logger\Logger as RoadRunnerLogger;
 
 class Handler extends AbstractProcessingHandler
 {
-    public function __construct(private readonly RoadRunnerLogger $logger)
-    {
+    public const FORMAT = "%message% %context% %extra%\n";
+
+    public function __construct(
+        private readonly RoadRunnerLogger $logger,
+        string|FormatterInterface $formatter = self::FORMAT
+    ) {
         parent::__construct();
+
+        if (\is_string($formatter)) {
+            $formatter = new LineFormatter($formatter);
+        }
+
+        $this->setFormatter($formatter);
     }
 
     protected function write(array $record): void
     {
         $message = $record['formatted'];
 
-        switch ($record['level']) {
-            case Logger::ERROR:
-                $this->logger->error($message);
-                break;
-            case Logger::WARNING:
-                $this->logger->warning($message);
-                break;
-            case Logger::INFO:
-                $this->logger->info($message);
-                break;
-            case Logger::DEBUG:
-                $this->logger->debug($message);
-                break;
-            default:
-                $this->logger->log($message);
-        }
+        match ($record['level']) {
+            Logger::ERROR, Logger::CRITICAL => $this->logger->error($message),
+            Logger::WARNING, Logger::ALERT, Logger::EMERGENCY => $this->logger->warning($message),
+            Logger::INFO, Logger::NOTICE => $this->logger->info($message),
+            Logger::DEBUG => $this->logger->debug($message),
+        };
     }
 }
