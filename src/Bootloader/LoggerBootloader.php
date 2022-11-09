3f5b0dd8c9b56d39d1a620a314940f352b41e7f1
Kirill Astakhov <kastahov1021@gmail.com>
2022-11-09 09:19:53 +0200

    Add roadrunner logger handler (#42)
    * Added support of roadrunner logger handler

---
+++ src/Bootloader/LoggerBootloader.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\RoadRunnerBridge\Bootloader;
+
+use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Boot\EnvironmentInterface;
+use Spiral\Monolog\Bootloader\MonologBootloader;
+use RoadRunner\Logger\Logger;
+use Spiral\RoadRunnerBridge\Logger\Handler;
+
+final class LoggerBootloader extends Bootloader
+{
+    protected const DEPENDENCIES = [
+        MonologBootloader::class
+    ];
+
+    protected const SINGLETONS = [
+        Handler::class => [self::class, 'initHandler']
+    ];
+
+    private function initHandler(Logger $logger, EnvironmentInterface $env): Handler
+    {
+        return new Handler($logger, $env->get('LOGGER_FORMAT', Handler::FORMAT));
+    }
+}
---
+++ tests/src/Logger/HandlerTest.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Logger;
+
+use Psr\Log\LoggerInterface;
+use RoadRunner\Logger\Logger;
+use Spiral\Goridge\RPC\RPCInterface;
+use Spiral\RoadRunnerBridge\Logger\Handler;
+use Spiral\Tests\TestCase;
+use Mockery as m;
+use Monolog\Logger as Monolog;
+
+final class HandlerTest extends TestCase
+{
+    /**
+     * @dataProvider provideLogData
+     */
+    public function testSendLog($expectedResult, $input): void
+    {
+        $rpc = m::mock(RPCInterface::class);
+
+        $rpc->shouldReceive('withServicePrefix')->once()->with('app')->andReturnSelf();
+
+        $monolog = new Monolog('default');
+        $monolog->setHandlers([
+            new Handler(
+                new Logger($rpc),
+                "%message% foo"
+            ),
+        ]);
+
+        $rpc->shouldReceive('call')
+            ->once()
+            ->with($expectedResult['level'], $expectedResult['message'])
+            ->andReturnSelf();
+
+        $method = $input['method'];
+
+        $monolog->$method($input['message']);
+    }
+
+    public function provideLogData(): array
+    {
+        return [
+            [
+                [
+                    'level' => 'Error',
+                    'message' => 'Error message foo',
+                ],
+                [
+                    'method' => 'error',
+                    'message' => 'Error message',
+                ],
+            ],
+            [
+                [
+                    'level' => 'Warning',
+                    'message' => 'Warning message foo',
+                ],
+                [
+                    'method' => 'warning',
+                    'message' => 'Warning message',
+                ],
+            ],
+            [
+                [
+                    'level' => 'Info',
+                    'message' => 'Info message foo',
+                ],
+                [
+                    'method' => 'info',
+                    'message' => 'Info message',
+                ],
+            ],
+            [
+                [
+                    'level' => 'Debug',
+                    'message' => 'Debug message foo',
+                ],
+                [
+                    'method' => 'debug',
+                    'message' => 'Debug message',
+                ],
+            ],
+            [
+                [
+                    'level' => 'Warning',
+                    'message' => 'Emergency message foo',
+                ],
+                [
+                    'method' => 'emergency',
+                    'message' => 'Emergency message',
+                ],
+            ],
+            [
+                [
+                    'level' => 'Warning',
+                    'message' => 'Alert message foo',
+                ],
+                [
+                    'method' => 'alert',
+                    'message' => 'Alert message',
+                ],
+            ],
+            [
+                [
+                    'level' => 'Error',
+                    'message' => 'Critical message foo',
+                ],
+                [
+                    'method' => 'critical',
+                    'message' => 'Critical message',
+                ],
+            ],
+            [
+                [
+                    'level' => 'Info',
+                    'message' => 'Notice message foo',
+                ],
+                [
+                    'method' => 'notice',
+                    'message' => 'Notice message',
+                ],
+            ],
+        ];
+    }
+}
