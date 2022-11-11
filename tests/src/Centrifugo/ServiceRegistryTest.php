2e8a5ab73feaaf0b632a7d767afff5ced04834de
Maxim Smakouz <m.smakouz@gmail.com>
2022-11-11 10:23:24 +0200

    Add tests for ServiceRegistry

---
+++ tests/src/Centrifugo/ServiceRegistryTest.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Centrifugo;
+
+use RoadRunner\Centrifugo\Request\RequestType;
+use Spiral\App\Centrifugo\TestService;
+use Spiral\Core\Container\Autowire;
+use Spiral\RoadRunnerBridge\Centrifugo\RegistryInterface;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceInterface;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceRegistry;
+use Spiral\Tests\TestCase;
+
+final class ServiceRegistryTest extends TestCase
+{
+    public function testGetServiceFromObject(): void
+    {
+        $this->updateConfig('centrifugo.services', [RequestType::Publish->value => new TestService()]);
+
+        $this->assertInstanceOf(TestService::class, $this->getService());
+    }
+
+    public function testGetServiceFromFCQN(): void
+    {
+        $this->updateConfig('centrifugo.services', [RequestType::Publish->value => TestService::class]);
+
+        $this->assertInstanceOf(TestService::class, $this->getService());
+    }
+
+    public function testGetServiceFromAlias(): void
+    {
+        $this->getContainer()->bind('alias', static fn () => new TestService());
+
+        $this->updateConfig('centrifugo.services', [RequestType::Publish->value => 'alias']);
+
+        $this->assertInstanceOf(TestService::class, $this->getService());
+    }
+
+    public function testGetServiceFromAutowire(): void
+    {
+        $this->updateConfig('centrifugo.services', [RequestType::Publish->value => new Autowire(TestService::class)]);
+
+        $this->assertInstanceOf(TestService::class, $this->getService());
+    }
+
+    /**
+     * @dataProvider servicesDataProvider
+     */
+    public function testRegister(Autowire|ServiceInterface|string $service): void
+    {
+        $this->getContainer()->bind('alias', new TestService());
+
+        $registry = new ServiceRegistry([], $this->getContainer(), $this->getContainer());
+
+        $this->assertNull($registry->getService(RequestType::Publish));
+
+        $registry->register(RequestType::Publish, $service);
+
+        $this->assertEquals(new TestService(), $registry->getService(RequestType::Publish));
+    }
+
+    public function servicesDataProvider(): \Traversable
+    {
+        yield [new Autowire(TestService::class)];
+        yield [new TestService()];
+        yield [TestService::class];
+        yield ['alias'];
+    }
+
+    private function getService(): ServiceInterface
+    {
+        return $this->getContainer()->get(RegistryInterface::class)->getService(RequestType::Publish);
+    }
+}
