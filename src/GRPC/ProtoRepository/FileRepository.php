4ecbde2eb5def5ebe871c97b7c4cc64edc4ef75f
kastahov <kastahov1021@gmail.com>
2022-11-16 21:38:12 +0200

    Fix code style

--- src/GRPC/ProtoRepository/FileRepository.php
+++ src/GRPC/ProtoRepository/FileRepository.php
@@ -6,8 +6,9 @@ namespace Spiral\RoadRunnerBridge\GRPC\ProtoRepository;
 
 final class FileRepository implements ProtoFilesRepositoryInterface
 {
-    public function __construct(private readonly array $protoFiles)
-    {
+    public function __construct(
+        private readonly array $protoFiles
+    ) {
     }
 
     public function getProtos(): iterable
