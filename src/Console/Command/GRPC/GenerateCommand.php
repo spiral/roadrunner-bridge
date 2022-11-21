88002ac97b1e739ef94613805f179046683bb514
Kirill Astakhov <kastahov1021@gmail.com>
2022-11-21 18:38:34 +0200

    Add new line separator in the end of `Protoc plugin binary was not found...`

--- src/Console/Command/GRPC/GenerateCommand.php
+++ src/Console/Command/GRPC/GenerateCommand.php
@@ -33,10 +33,7 @@ final class GenerateCommand extends Command
         $binaryPath = $config->getBinaryPath();
 
         if ($binaryPath !== null && !\file_exists($binaryPath)) {
-            $this->sprintf(
-                '<error>Protoc plugin binary `%s` was not found.  Use command `./vendor/bin/rr download-protoc-binary` to download it.`</error>',
-                $binaryPath
-            );
+            $this->error("Protoc plugin binary `$binaryPath` was not found. Use command `./vendor/bin/rr download-protoc-binary` to download it.");
 
             return self::FAILURE;
         }
