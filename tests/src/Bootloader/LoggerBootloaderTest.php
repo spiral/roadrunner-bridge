d3704ebdf5dc602b7c1380ed1cdd5892350074db
butschster <butschster@gmail.com>
2023-01-25 23:59:03 +0400

    LoggerBootloader will register roadrunner app logger handler automatically after registering it

--- tests/src/Bootloader/LoggerBootloaderTest.php
+++ tests/src/Bootloader/LoggerBootloaderTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Bootloader;
 
+use Spiral\Monolog\Config\MonologConfig;
 use Spiral\RoadRunnerBridge\Logger\Handler;
 use Spiral\Tests\TestCase;
 
@@ -12,5 +13,13 @@ final class LoggerBootloaderTest extends TestCase
     public function testHandlerBinding(): void
     {
         $this->assertContainerBoundAsSingleton(Handler::class, Handler::class);
+    }
+
+    public function testHandlerIsRegisteredInMonolog(): void
+    {
+        $config = $this->getConfig(MonologConfig::CONFIG);
+
+        $this->assertArrayHasKey('roadrunner', $config['handlers']);
+        $this->assertInstanceOf(Handler::class, $config['handlers']['roadrunner'][0]);
     }
 }
