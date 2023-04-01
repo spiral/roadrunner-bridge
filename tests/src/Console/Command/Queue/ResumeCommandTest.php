985b2b5dcdfa884216121e7fda0eda677f2d8494
butschster <butschster@gmail.com>
2023-04-01 12:11:28 +0400

    Resume console command declares a pipeline if it wasn't declared and exists in config

--- tests/src/Console/Command/Queue/ResumeCommandTest.php
+++ tests/src/Console/Command/Queue/ResumeCommandTest.php
@@ -4,19 +4,39 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Console\Command\Queue;
 
-use Spiral\RoadRunner\Jobs\JobsInterface;
+use Mockery as m;
+use Spiral\RoadRunner\Jobs\QueueInterface;
+use Spiral\RoadRunnerBridge\Queue\PipelineRegistryInterface;
 use Spiral\Tests\ConsoleTestCase;
 
 final class ResumeCommandTest extends ConsoleTestCase
 {
-    public function testPausePipeline()
+    public function testResumePipeline()
     {
-        $jobs = \Mockery::mock(JobsInterface::class);
-        $this->getContainer()->bind(JobsInterface::class, $jobs);
+        $registry = $this->mockContainer(PipelineRegistryInterface::class);
 
-        $jobs->shouldReceive('resume')->once()->with('foo');
+        $registry->shouldReceive('getPipeline')->once()->with('foo')->andReturn(
+            $queue = m::mock(QueueInterface::class),
+        );
+
+        $queue->shouldReceive('isPaused')->once()->andReturnTrue();
+        $queue->shouldReceive('resume')->once();
 
         $result = $this->runCommand('rr:jobs:consume', ['pipeline' => 'foo']);
-        $this->assertStringContainsString('Pipeline [foo] has been resumed.', $result);
+        $this->assertStringContainsString('Pipeline [foo] has been started consuming tasks.', $result);
+    }
+
+    public function testResumeIsConsumingPipeline()
+    {
+        $registry = $this->mockContainer(PipelineRegistryInterface::class);
+
+        $registry->shouldReceive('getPipeline')->once()->with('foo')->andReturn(
+            $queue = m::mock(QueueInterface::class),
+        );
+
+        $queue->shouldReceive('isPaused')->once()->andReturnFalse();
+
+        $result = $this->runCommand('rr:jobs:consume', ['pipeline' => 'foo']);
+        $this->assertStringContainsString('Pipeline [foo] is not paused.', $result);
     }
 }
