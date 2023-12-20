adbadd0b1293a8a513cf0b868726c45b81dcb1bb
butschster <butschster@gmail.com>
2023-12-20 17:36:20 +0400

    Improves Scaffolder for Centrifugo handler

--- src/Console/Command/Scaffolder/CentrifugoHandlerCommand.php
+++ src/Console/Command/Scaffolder/CentrifugoHandlerCommand.php
@@ -24,12 +24,145 @@ final class CentrifugoHandlerCommand extends AbstractCommand
     #[Option(description: 'Optional, specify a custom namespace')]
     private ?string $namespace = null;
 
+    #[Option(shortcut: 't', description: 'Service type [connect, subscribe, rpc, refresh, publish]')]
+    private string $type = 'connect';
+
+    #[Option(name: 'with-api', shortcut: 'a', description: 'With API dependency')]
+    private bool $withApi = false;
+
     public function perform(): int
     {
-        $declaration = $this->createDeclaration(CentrifugoHandlerDeclaration::class);
+        $declaration = $this->createDeclaration(CentrifugoHandlerDeclaration::class, [
+            'withApi' => $this->withApi,
+        ]);
+
+        $type = match ($this->type) {
+            'connect' => [
+                'request' => \RoadRunner\Centrifugo\Request\Connect::class,
+                'use' => [
+                    \RoadRunner\Centrifugo\Payload\ConnectResponse::class,
+                ],
+                'body' => $this->createConnectHandlerBody(),
+            ],
+            'subscribe' => [
+                'request' => \RoadRunner\Centrifugo\Request\Subscribe::class,
+                'use' => [
+                    \RoadRunner\Centrifugo\Payload\SubscribeResponse::class,
+                ],
+                'body' => $this->createSubscribeHandlerBody(),
+            ],
+            'rpc' => [
+                'request' => \RoadRunner\Centrifugo\Request\RPC::class,
+                'use' => [
+                    \RoadRunner\Centrifugo\Payload\RPCResponse::class,
+                ],
+                'body' => $this->createRpcHandlerBody(),
+            ],
+            'refresh' => [
+                'request' => \RoadRunner\Centrifugo\Request\Refresh::class,
+                'use' => [
+                    \RoadRunner\Centrifugo\Payload\RefreshResponse::class,
+                ],
+                'body' => $this->createRefreshHandlerBody(),
+            ],
+            'publish' => [
+                'request' => \RoadRunner\Centrifugo\Request\Publish::class,
+                'use' => [
+                    \RoadRunner\Centrifugo\Payload\PublishResponse::class,
+                ],
+                'body' => $this->createPublishHandlerBody(),
+            ],
+            default => throw new \InvalidArgumentException('Invalid service type'),
+        };
+
+        $declaration->setType($type);
 
         $this->writeDeclaration($declaration);
 
         return self::SUCCESS;
+    }
+
+    private function createConnectHandlerBody(): string
+    {
+        return <<<'PHP'
+try {
+    $request->respond(
+        new ConnectResponse(
+            user: '', // User ID
+            channels: [
+                // List of channels to subscribe to on connect to Centrifugo
+                // 'public',
+            ],
+        )
+    );
+} catch (\Throwable $e) {
+    $request->error($e->getCode(), $e->getMessage());
+}
+PHP;
+    }
+
+    private function createSubscribeHandlerBody(): string
+    {
+        return <<<'PHP'
+try {
+    // Here you can check if user is allowed to subscribe to requested channel
+    if ($request->channel !== 'public') {
+        $request->disconnect('403', 'Channel is not allowed.');
+        return;
+    }
+
+    $request->respond(
+        new SubscribeResponse()
+    );
+} catch (\Throwable $e) {
+    $request->error($e->getCode(), $e->getMessage());
+}
+PHP;
+    }
+
+    private function createRpcHandlerBody(): string
+    {
+        return <<<'PHP'
+$result = match ($request->method) {
+    'ping' => ['pong' => 'pong', 'code' => 200],
+    default => ['error' => 'Not found', 'code' => 404]
+};
+
+try {
+    $request->respond(
+        new RPCResponse(
+            data: $result
+        )
+    );
+} catch (\Throwable $e) {
+    $request->error($e->getCode(), $e->getMessage());
+}
+PHP;
+    }
+
+    private function createRefreshHandlerBody(): string
+    {
+        return <<<'PHP'
+try {
+    $request->respond(
+        new RefreshResponse(...)
+    );
+} catch (\Throwable $e) {
+    $request->error($e->getCode(), $e->getMessage());
+}
+PHP;
+    }
+
+    private function createPublishHandlerBody(): string
+    {
+        return <<<'PHP'
+try {
+    $request->respond(
+        new PublishResponse(...)
+    );
+} catch (\Throwable $e) {
+    $request->error($e->getCode(), $e->getMessage());
+}
+PHP;
     }
 }
--- tests/src/Console/Command/Scaffolder/CentrifugoHandlerCommandTest.php
+++ tests/src/Console/Command/Scaffolder/CentrifugoHandlerCommandTest.php
@@ -23,20 +23,243 @@ declare(strict_types=1);
 
 namespace Spiral\Testing\Endpoint\Centrifugo\Handler;
 
-use RoadRunner\Centrifugo\CentrifugoApiInterface;
+use RoadRunner\Centrifugo\Payload\ConnectResponse;
+use RoadRunner\Centrifugo\Request\Connect;
+use RoadRunner\Centrifugo\Request\RequestInterface;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceInterface;
 
 /**
  * Sample Handler
  */
-final class SampleHandler
+final class SampleHandler implements ServiceInterface
 {
-    public function __construct(
-        private readonly CentrifugoApiInterface $api,
-    ) {
+    /**
+     * @param Connect $request
+     */
+    public function handle(RequestInterface $request): void
+    {
+        try {
+            $request->respond(
+                new ConnectResponse(
+                    user: '', // User ID
+                    channels: [
+                        // List of channels to subscribe to on connect to Centrifugo
+                        // 'public',
+                    ],
+                )
+            );
+        } catch (\Throwable $e) {
+            $request->error($e->getCode(), $e->getMessage());
+        }
+    }
+}
+
+PHP,
+            expectedFilename: 'app/src/Endpoint/Centrifugo/Handler/SampleHandler.php',
+            expectedOutputStrings: [
+                "SampleHandler' has been successfully written into 'app/src/Endpoint/Centrifugo/Handler/SampleHandler.php",
+            ],
+        );
     }
 
-    public function handle(): void
+    public function testScaffoldSubscribe(): void
     {
+        $this->assertScaffolderCommandSame(
+            command: 'create:centrifugo-handler',
+            args: [
+                'name' => 'sample',
+                '--type' => 'subscribe',
+                '--comment' => 'Sample Handler',
+            ],
+            expected: <<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Testing\Endpoint\Centrifugo\Handler;
+
+use RoadRunner\Centrifugo\Payload\SubscribeResponse;
+use RoadRunner\Centrifugo\Request\RequestInterface;
+use RoadRunner\Centrifugo\Request\Subscribe;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceInterface;
+
+/**
+ * Sample Handler
+ */
+final class SampleHandler implements ServiceInterface
+{
+    /**
+     * @param Subscribe $request
+     */
+    public function handle(RequestInterface $request): void
+    {
+        try {
+            // Here you can check if user is allowed to subscribe to requested channel
+            if ($request->channel !== 'public') {
+                $request->disconnect('403', 'Channel is not allowed.');
+                return;
+            }
+
+            $request->respond(
+                new SubscribeResponse()
+            );
+        } catch (\Throwable $e) {
+            $request->error($e->getCode(), $e->getMessage());
+        }
+    }
+}
+
+PHP,
+            expectedFilename: 'app/src/Endpoint/Centrifugo/Handler/SampleHandler.php',
+            expectedOutputStrings: [
+                "SampleHandler' has been successfully written into 'app/src/Endpoint/Centrifugo/Handler/SampleHandler.php",
+            ],
+        );
+    }
+
+    public function testScaffoldRpc(): void
+    {
+        $this->assertScaffolderCommandSame(
+            command: 'create:centrifugo-handler',
+            args: [
+                'name' => 'sample',
+                '--type' => 'rpc',
+                '--comment' => 'Sample Handler',
+            ],
+            expected: <<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Testing\Endpoint\Centrifugo\Handler;
+
+use RoadRunner\Centrifugo\Payload\RPCResponse;
+use RoadRunner\Centrifugo\Request\RPC;
+use RoadRunner\Centrifugo\Request\RequestInterface;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceInterface;
+
+/**
+ * Sample Handler
+ */
+final class SampleHandler implements ServiceInterface
+{
+    /**
+     * @param RPC $request
+     */
+    public function handle(RequestInterface $request): void
+    {
+        $result = match ($request->method) {
+            'ping' => ['pong' => 'pong', 'code' => 200],
+            default => ['error' => 'Not found', 'code' => 404]
+        };
+
+        try {
+            $request->respond(
+                new RPCResponse(
+                    data: $result
+                )
+            );
+        } catch (\Throwable $e) {
+            $request->error($e->getCode(), $e->getMessage());
+        }
+    }
+}
+
+PHP,
+            expectedFilename: 'app/src/Endpoint/Centrifugo/Handler/SampleHandler.php',
+            expectedOutputStrings: [
+                "SampleHandler' has been successfully written into 'app/src/Endpoint/Centrifugo/Handler/SampleHandler.php",
+            ],
+        );
+    }
+
+    public function testScaffoldRefresh(): void
+    {
+        $this->assertScaffolderCommandSame(
+            command: 'create:centrifugo-handler',
+            args: [
+                'name' => 'sample',
+                '--type' => 'refresh',
+                '--comment' => 'Sample Handler',
+            ],
+            expected: <<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Testing\Endpoint\Centrifugo\Handler;
+
+use RoadRunner\Centrifugo\Payload\RefreshResponse;
+use RoadRunner\Centrifugo\Request\Refresh;
+use RoadRunner\Centrifugo\Request\RequestInterface;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceInterface;
+
+/**
+ * Sample Handler
+ */
+final class SampleHandler implements ServiceInterface
+{
+    /**
+     * @param Refresh $request
+     */
+    public function handle(RequestInterface $request): void
+    {
+        try {
+            $request->respond(
+                new RefreshResponse(...)
+            );
+        } catch (\Throwable $e) {
+            $request->error($e->getCode(), $e->getMessage());
+        }
+    }
+}
+
+PHP,
+            expectedFilename: 'app/src/Endpoint/Centrifugo/Handler/SampleHandler.php',
+            expectedOutputStrings: [
+                "SampleHandler' has been successfully written into 'app/src/Endpoint/Centrifugo/Handler/SampleHandler.php",
+            ],
+        );
+    }
+
+    public function testScaffoldPublish(): void
+    {
+        $this->assertScaffolderCommandSame(
+            command: 'create:centrifugo-handler',
+            args: [
+                'name' => 'sample',
+                '--type' => 'publish',
+                '--comment' => 'Sample Handler',
+            ],
+            expected: <<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Testing\Endpoint\Centrifugo\Handler;
+
+use RoadRunner\Centrifugo\Payload\PublishResponse;
+use RoadRunner\Centrifugo\Request\Publish;
+use RoadRunner\Centrifugo\Request\RequestInterface;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceInterface;
+
+/**
+ * Sample Handler
+ */
+final class SampleHandler implements ServiceInterface
+{
+    /**
+     * @param Publish $request
+     */
+    public function handle(RequestInterface $request): void
+    {
+        try {
+            $request->respond(
+                new PublishResponse(...)
+            );
+        } catch (\Throwable $e) {
+            $request->error($e->getCode(), $e->getMessage());
+        }
     }
 }
 
@@ -63,17 +286,89 @@ declare(strict_types=1);
 
 namespace Spiral\Testing\Endpoint\Centrifugo\Other;
 
-use RoadRunner\Centrifugo\CentrifugoApiInterface;
+use RoadRunner\Centrifugo\Payload\ConnectResponse;
+use RoadRunner\Centrifugo\Request\Connect;
+use RoadRunner\Centrifugo\Request\RequestInterface;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceInterface;
 
-final class SampleHandler
+final class SampleHandler implements ServiceInterface
+{
+    /**
+     * @param Connect $request
+     */
+    public function handle(RequestInterface $request): void
+    {
+        try {
+            $request->respond(
+                new ConnectResponse(
+                    user: '', // User ID
+                    channels: [
+                        // List of channels to subscribe to on connect to Centrifugo
+                        // 'public',
+                    ],
+                )
+            );
+        } catch (\Throwable $e) {
+            $request->error($e->getCode(), $e->getMessage());
+        }
+    }
+}
+
+PHP,
+            expectedFilename: 'app/src/Endpoint/Centrifugo/Other/SampleHandler.php',
+            expectedOutputStrings: [
+                "SampleHandler' has been successfully written into 'app/src/Endpoint/Centrifugo/Other/SampleHandler.php",
+            ],
+        );
+    }
+
+    public function testScaffoldWithApi(): void
+    {
+        $this->assertScaffolderCommandSame(
+            command: 'create:centrifugo-handler',
+            args: [
+                'name' => 'sample',
+                '--with-api' => true,
+                '--namespace' => 'Spiral\\Testing\\Endpoint\\Centrifugo\\Other',
+            ],
+            expected: <<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Testing\Endpoint\Centrifugo\Other;
+
+use RoadRunner\Centrifugo\CentrifugoApiInterface;
+use RoadRunner\Centrifugo\Payload\ConnectResponse;
+use RoadRunner\Centrifugo\Request\Connect;
+use RoadRunner\Centrifugo\Request\RequestInterface;
+use Spiral\RoadRunnerBridge\Centrifugo\ServiceInterface;
+
+final class SampleHandler implements ServiceInterface
 {
     public function __construct(
         private readonly CentrifugoApiInterface $api,
     ) {
     }
 
-    public function handle(): void
+    /**
+     * @param Connect $request
+     */
+    public function handle(RequestInterface $request): void
     {
+        try {
+            $request->respond(
+                new ConnectResponse(
+                    user: '', // User ID
+                    channels: [
+                        // List of channels to subscribe to on connect to Centrifugo
+                        // 'public',
+                    ],
+                )
+            );
+        } catch (\Throwable $e) {
+            $request->error($e->getCode(), $e->getMessage());
+        }
     }
 }
 
