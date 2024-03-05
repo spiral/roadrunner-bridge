c1e4b538f92b519709869bd49c2d633fcf55228c
Maxim Smakouz <m.smakouz@gmail.com>
2024-03-05 13:06:36 +0200

    Revert ServiceLocator changes

--- src/GRPC/ServiceLocator.php
+++ src/GRPC/ServiceLocator.php
@@ -5,9 +5,6 @@ declare(strict_types=1);
 namespace Spiral\RoadRunnerBridge\GRPC;
 
 use Psr\Container\ContainerInterface;
-use Spiral\Core\BinderInterface;
-use Spiral\Core\Attribute\Proxy as ProxyAttribute;
-use Spiral\Core\Config\Proxy;
 use Spiral\Core\Exception\Container\ContainerException;
 use Spiral\RoadRunner\GRPC\ServiceInterface;
 use Spiral\Tokenizer\ClassesInterface;
@@ -16,8 +13,7 @@ final class ServiceLocator implements LocatorInterface
 {
     public function __construct(
         private readonly ClassesInterface $classes,
-        #[ProxyAttribute] private readonly ContainerInterface $container,
-        private readonly BinderInterface $binder,
+        private readonly ContainerInterface $container
     ) {
     }
 
@@ -30,19 +26,14 @@ final class ServiceLocator implements LocatorInterface
                 continue;
             }
 
+            try {
+                $instance = $this->container->get($service->getName());
+            } catch (ContainerException) {
+                continue;
+            }
+
             foreach ($service->getInterfaces() as $interface) {
                 if ($interface->isSubclassOf(ServiceInterface::class)) {
-                    $grpcRequest = $this->binder->getBinder('grpc.request');
-                    $grpcRequest->bind($interface->getName(), $service->getName());
-                    $grpcRequest->bind($service->getName(), $service->getName());
-                    $this->binder->bind($interface->getName(), new Proxy($interface->getName()));
-
-                    try {
-                        $instance = $this->container->get($interface->getName());
-                    } catch (ContainerException) {
-                        continue;
-                    }
-
                     $result[$interface->getName()] = $instance;
                 }
             }
