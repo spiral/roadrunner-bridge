87c48b559769ecdf08558e114ede1a91a34a3cbe
Sergey Syomkin <sergey.syomkin@bestchange.dev>
2025-02-26 12:48:57 +0300

    cs:fix

--- tests/src/Bootloader/LoggerBootloaderTest.php
+++ tests/src/Bootloader/LoggerBootloaderTest.php
@@ -5,17 +5,7 @@ declare(strict_types=1);
 namespace Spiral\Tests\Bootloader;
 
 use Monolog\Handler\ErrorLogHandler;
-use RoadRunner\Logger\Logger;
-use Spiral\Boot\EnvironmentInterface;
-use Spiral\Boot\FinalizerInterface;
-use Spiral\Config\ConfiguratorInterface;
-use Spiral\Goridge\RPC\RPCInterface;
-use Spiral\Monolog\Bootloader\MonologBootloader;
 use Spiral\Monolog\Config\MonologConfig;
-use Spiral\RoadRunnerBridge\Bootloader\LoggerBootloader;
-use Spiral\RoadRunnerBridge\Logger\Handler;
-use Spiral\RoadRunnerBridge\Logger\RoadRunnerLogsMode;
-use Spiral\RoadRunnerBridge\RoadRunnerMode;
 use Spiral\Tests\TestCase;
 
 /**
